64489cae61652ecc8106781e6897f72a45e4615f
chosule <89799325+chosule@users.noreply.github.com>
2023-02-25 16:59:46 +0900

    next_router

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,33 +1,36 @@
 import React from "react";
-import {Grid} from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import styles from "./ItemList.module.css";
-import Link from "next/link"; 
+import Link from "next/link";
 
-
-
-export default function ItemList({list}){
-    return (
-        <div>
-            <Grid columns={3}>
-                <Grid.Row> 
-                    {list.map((item) =>(
-                        <Grid.Column key={item.id}>
-                            <Link href={`/view/${item.id}`}>
-                                <Grid.Column>
-                                    <div className={styles.wrap}>
-                                        <img src={item.image_link} alt={item.name} className={styles.img_item}/>
-                                        <strong className={styles.tit_item}>{item.name}</strong>
-                                        <strong className={styles.num_price}>${item.price}</strong>
-                                        <span className={styles.txt_info}>
-                                            {item.category}{item.product_type}
-                                        </span>
-                                    </div>
-                                </Grid.Column>
-                            </Link>
-                        </Grid.Column>
-                    ))}
-                </Grid.Row>
-            </Grid>
-        </div>
-    )
+export default function ItemList({ list }) {
+  return (
+    <div>
+      <Grid columns={3}>
+        <Grid.Row>
+          {list.map((item) => (
+            <Grid.Column key={item.id}>
+              <Link href="/view/[id]" as={`/view/${item.id}`}>
+                <Grid.Column>
+                  <div className={styles.wrap}>
+                    <img
+                      src={item.image_link}
+                      alt={item.name}
+                      className={styles.img_item}
+                    />
+                    <strong className={styles.tit_item}>{item.name}</strong>
+                    <strong className={styles.num_price}>${item.price}</strong>
+                    <span className={styles.txt_info}>
+                      {item.category}
+                      {item.product_type}
+                    </span>
+                  </div>
+                </Grid.Column>
+              </Link>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
--- src/pages/view/[id].js
+++ src/pages/view/[id].js
@@ -1,45 +1,37 @@
-import { useRouter } from 'next/router'
-import { useEffect,useState } from 'react';
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Axios from "axios";
-import Item from "../../components/Item"
-import {Loader } from "semantic-ui-react";
+import Item from "../../components/Item";
+import { Loader } from "semantic-ui-react";
+import Head from "next/head";
 
-const Post = () => {
-  const router = useRouter()
-  const { id } = router.query
-  
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`
-
-  const [item , setItem ] = useState({});
-  const [isLoading , setIsLoading ] = useState(true)
-
-  function getData(){
-    Axios.get(API_URL)
-    .then(res =>{
-        setItem(res.data)
-        setIsLoading(false)
-    })
-  }
-
-  useEffect(() =>{
-    if( id && id > 0){
-        getData();
-    }
-  },[id])
-//   console.log(item, 'test')
-  return(
+const Post = ({ item }) => {
+  return (
     <>
-        {isLoading ? (
-                <div style={{padding:"300px 0"}}>
-                    <Loader inline="centered" active>Loading...</Loader>
-                </div>
-            ) : (            
-                <Item item={item}/>
-            )
-        }
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item}></Item>
+        </>
+      )}
     </>
-  )
+  );
+};
+
+export default Post;
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+    },
+  };
 }
-
-export default Post
-
